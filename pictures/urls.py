92cf8d2306c3be9cca7fa54830955411fca7a62b
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-29 02:14:21 +0300

    image view function

--- pictures/urls.py
+++ pictures/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns=[
     path('',views.index, name='home'),
+    path('images/',views.images,name='images'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- pictures/views.py
+++ pictures/views.py
@@ -1,6 +1,13 @@
+from unicodedata import category
 from django.shortcuts import render
 from django.http  import HttpResponse
-
+from .models import Category,Location,Image
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def images(request):
+    images = Image.objects.all()
+    category = Category.objects.all()
+    location = Location.objects.all()
+    return render(request,'images.html',{'images':images,'category':category,'location':location})
