61dff6c113cc5ad64ed0e16ce87c61e9e106dd37
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-29 17:18:20 +0300

    Add search & location url paths

--- pictures/urls.py
+++ pictures/urls.py
@@ -6,6 +6,8 @@ from django.conf import settings
 urlpatterns=[
     path('',views.index, name='home'),
     path('images/',views.images,name='images'),
+    path('search/', views.search_results, name='search_results'),
+    path('location/',views.get_location,name='location'),
 ]
 
 if settings.DEBUG:
