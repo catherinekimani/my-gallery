d49579fe98f8fcd05efc07dc7367c723bca814a3
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-05-29 17:16:53 +0300

    Create search function to enable search for image categories

--- pictures/views.py
+++ pictures/views.py
@@ -2,6 +2,7 @@ from unicodedata import category
 from django.shortcuts import render
 from django.http  import HttpResponse
 from .models import Category,Location,Image
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -11,3 +12,16 @@ def images(request):
     category = Category.objects.all()
     location = Location.objects.all()
     return render(request,'images.html',{'images':images,'category':category,'location':location})
+
+def search_results(request):
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET["category"]
+        searched_images = Image.search_by_category(search_term)
+        message = f"{search_term}"
+        
+        return render(request, 'search.html', {"message":message, "images":searched_images})
+    return render(request,'search.html')
+
+def get_location(request,location_id):
+    images=Image.filter_by_location(location_id)
+    return render(request, 'location.html', {"images":images})
